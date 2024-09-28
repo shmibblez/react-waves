5eac3bb6a6023f1ef1f7136c0050a645fd09fecc
shmibblez <watchouthomes@gmail.com>
2024-09-28 05:07:13 -0500

    added chord shape generator, untested

--- src/tools/chordMaster.ts
+++ src/tools/chordMaster.ts
@@ -91,11 +91,16 @@ for (const k of keys) {
 // noteToFreq map
 console.log("const noteToFreq = " + JSON.stringify(noteToFreq))
 
+// SupportedNotes type
+console.log("type SupportedNote = " + JSON.stringify(notes.flat(2)).replace(/,/g, " |").replace(/[\[\]]/g,""))
+
+// Supported Frequencies type
+console.log("type SupportedFrequency = " + JSON.stringify(t).replace(/,/g, " |").replace(/[\[\]]/g,""))
 
  */
 
 class Maps {
-  static notes = [
+  static notes: (SupportedNote | [SupportedNote, SupportedNote])[] = [
     "C0",
     ["C#0", "Db0"],
     "D0",
@@ -205,7 +210,7 @@ class Maps {
     ["A#8", "Bb8"],
     "B8",
   ];
-  static sharps = [
+  static sharps: SupportedNote[] = [
     "C0",
     "C#0",
     "D0",
@@ -315,7 +320,7 @@ class Maps {
     "A#8",
     "B8",
   ];
-  static flats = [
+  static flats: SupportedNote[] = [
     "C0",
     "Db0",
     "D0",
@@ -438,7 +443,7 @@ class Maps {
     2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951, 4186, 4434.92, 4698.63,
     4978, 5274, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13,
   ];
-  static freqS = [
+  static freqS: SupportedFrequency[] = [
     "16.35",
     "17.32",
     "18.35",
@@ -548,7 +553,9 @@ class Maps {
     "7458.62",
     "7902.13",
   ];
-  static freqToNote = {
+  static freqToNote: {
+    [f in SupportedFrequency]: SupportedNote | [SupportedNote, SupportedNote];
+  } = {
     "16.35": "C0",
     "17.32": ["C#0", "Db0"],
     "18.35": "D0",
@@ -658,7 +665,7 @@ class Maps {
     "7458.62": ["A#8", "Bb8"],
     "7902.13": "B8",
   };
-  static noteToFreq = {
+  static noteToFreq: { [n in SupportedNote]: SupportedFrequency } = {
     C0: "16.35",
     "C#0": "17.32",
     Db0: "17.32",
@@ -1786,7 +1793,274 @@ class Maps {
     // base item:
     // "": { short: "", long: "", fullName: "" },
   };
+
+  static chordPatternDuplicates = {};
 }
+
+type SupportedNote =
+  | "C0"
+  | "C#0"
+  | "Db0"
+  | "D0"
+  | "D#0"
+  | "Eb0"
+  | "E0"
+  | "F0"
+  | "F#0"
+  | "Gb0"
+  | "G0"
+  | "G#0"
+  | "Ab0"
+  | "A0"
+  | "A#0"
+  | "Bb0"
+  | "B0"
+  | "C1"
+  | "C#1"
+  | "Db1"
+  | "D1"
+  | "D#1"
+  | "Eb1"
+  | "E1"
+  | "F1"
+  | "F#1"
+  | "Gb1"
+  | "G1"
+  | "G#1"
+  | "Ab1"
+  | "A1"
+  | "A#1"
+  | "Bb1"
+  | "B1"
+  | "C2"
+  | "C#2"
+  | "Db2"
+  | "D2"
+  | "D#2"
+  | "Eb2"
+  | "E2"
+  | "F2"
+  | "F#2"
+  | "Gb2"
+  | "G2"
+  | "G#2"
+  | "Ab2"
+  | "A2"
+  | "A#2"
+  | "Bb2"
+  | "B2"
+  | "C3"
+  | "C#3"
+  | "Db3"
+  | "D3"
+  | "D#3"
+  | "Eb3"
+  | "E3"
+  | "F3"
+  | "F#3"
+  | "Gb3"
+  | "G3"
+  | "G#3"
+  | "Ab3"
+  | "A3"
+  | "A#3"
+  | "Bb3"
+  | "B3"
+  | "C4"
+  | "C#4"
+  | "Db4"
+  | "D4"
+  | "D#4"
+  | "Eb4"
+  | "E4"
+  | "F4"
+  | "F#4"
+  | "Gb4"
+  | "G4"
+  | "G#4"
+  | "Ab4"
+  | "A4"
+  | "A#4"
+  | "Bb4"
+  | "B4"
+  | "C5"
+  | "C#5"
+  | "Db5"
+  | "D5"
+  | "D#5"
+  | "Eb5"
+  | "E5"
+  | "F5"
+  | "F#5"
+  | "Gb5"
+  | "G5"
+  | "G#5"
+  | "Ab5"
+  | "A5"
+  | "A#5"
+  | "Bb5"
+  | "B5"
+  | "C6"
+  | "C#6"
+  | "Db6"
+  | "D6"
+  | "D#6"
+  | "Eb6"
+  | "E6"
+  | "F6"
+  | "F#6"
+  | "Gb6"
+  | "G6"
+  | "G#6"
+  | "Ab6"
+  | "A6"
+  | "A#6"
+  | "Bb6"
+  | "B6"
+  | "C7"
+  | "C#7"
+  | "Db7"
+  | "D7"
+  | "D#7"
+  | "Eb7"
+  | "E7"
+  | "F7"
+  | "F#7"
+  | "Gb7"
+  | "G7"
+  | "G#7"
+  | "Ab7"
+  | "A7"
+  | "A#7"
+  | "Bb7"
+  | "B7"
+  | "C8"
+  | "C#8"
+  | "Db8"
+  | "D8"
+  | "D#8"
+  | "Eb8"
+  | "E8"
+  | "F8"
+  | "F#8"
+  | "Gb8"
+  | "G8"
+  | "G#8"
+  | "Ab8"
+  | "A8"
+  | "A#8"
+  | "Bb8"
+  | "B8";
+
+type SupportedFrequency =
+  | "16.35"
+  | "17.32"
+  | "18.35"
+  | "19.45"
+  | "20.60"
+  | "21.83"
+  | "23.12"
+  | "24.50"
+  | "25.96"
+  | "27.50"
+  | "29.14"
+  | "30.87"
+  | "32.70"
+  | "34.65"
+  | "36.71"
+  | "38.89"
+  | "41.20"
+  | "43.65"
+  | "46.25"
+  | "49.00"
+  | "51.91"
+  | "55.00"
+  | "58.27"
+  | "61.74"
+  | "65.41"
+  | "69.30"
+  | "73.42"
+  | "77.78"
+  | "82.41"
+  | "87.31"
+  | "92.50"
+  | "98.00"
+  | "103.83"
+  | "110.00"
+  | "116.54"
+  | "123.47"
+  | "130.81"
+  | "138.59"
+  | "146.83"
+  | "155.56"
+  | "164.81"
+  | "174.61"
+  | "185.00"
+  | "196.00"
+  | "207.65"
+  | "220.00"
+  | "233.08"
+  | "246.94"
+  | "261.63"
+  | "277.18"
+  | "293.66"
+  | "311.13"
+  | "329.63"
+  | "349.23"
+  | "369.99"
+  | "392.00"
+  | "415.30"
+  | "440.00"
+  | "466.16"
+  | "493.88"
+  | "523.25"
+  | "554.37"
+  | "587.33"
+  | "622.25"
+  | "659.25"
+  | "698.46"
+  | "739.99"
+  | "783.99"
+  | "830.61"
+  | "880.00"
+  | "932.33"
+  | "987.77"
+  | "1046.50"
+  | "1108.73"
+  | "1174.66"
+  | "1244.51"
+  | "1318.51"
+  | "1396.91"
+  | "1479.98"
+  | "1567.98"
+  | "1661.22"
+  | "1760.00"
+  | "1864.66"
+  | "1975.53"
+  | "2093.00"
+  | "2217.46"
+  | "2349.32"
+  | "2489.00"
+  | "2637.00"
+  | "2793.83"
+  | "2959.96"
+  | "3135.96"
+  | "3322.44"
+  | "3520.00"
+  | "3729.31"
+  | "3951.00"
+  | "4186.00"
+  | "4434.92"
+  | "4698.63"
+  | "4978.00"
+  | "5274.00"
+  | "5587.65"
+  | "5919.91"
+  | "6271.93"
+  | "6644.88"
+  | "7040.00"
+  | "7458.62"
+  | "7902.13";
 
 // export class ChordMaster {
 //   public static decipherFrequencies(frequencies: (string | number)[]): Chord {
@@ -1817,7 +2091,7 @@ export class Chord {
    * @throws error if letter not supported (ex: C#9, Ab20)
    * @returns Note
    */
-  public static fromLetters(letters: string[]) {
+  public static fromLetters(letters: SupportedNote[]) {
     let notes: Note[] = [];
     // flatten notes array to contain flats and sharps
     for (const l of letters) {
@@ -1881,6 +2155,83 @@ export class Chord {
     // if interval found return
     return i.long;
   }
+
+  /**
+   * returns list of chord shapes for given chord, could return unplayable chords
+   * @param tuning tuning of guitar, default is standard tuning (size of array changes number of strings)
+   * @returns array of possible chord voicings (shapes), each shape is an array of fret number or "x", in order of strings passed
+   */
+  public guitarChordShapes(
+    tuning: SupportedNote[] = ["E2", "A2", "D3", "G3", "B3", "E4"]
+  ): ChordShapeGuitar[] {
+    // TODO:
+    //  - create function that generates list of all possible
+    //    combos in string representation, order doesnt matter
+    //  - create loop that iterates over, interprets each combo
+    //    and generates shape for each
+    //  - discard shape if negative fret number (note is lower
+    //    than note on lowest fret)
+
+    // from: https://stackoverflow.com/a/20871714
+    // create all chord permutations possible
+    function permutator<item>(inputArr: item[]): item[][] {
+      let result = [];
+      const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+          result.push(m);
+        } else {
+          for (let i = 0; i < arr.length; i++) {
+            let curr = arr.slice();
+            let next = curr.splice(i, 1);
+            permute(curr.slice(), m.concat(next));
+          }
+        }
+      };
+      permute(inputArr);
+      return result;
+    }
+
+    // array of chord letters
+    const notesOG = this.notes.map((v) => v.sharp as string);
+    // add x's until notesOG is same length as tuning
+    while (notesOG.length < tuning.length) {
+      notesOG.push("x");
+    }
+    // list of all note permutations
+    const permutations = permutator(notesOG);
+    // list of all chord chord shapes / fingerings possible
+    const shapes: ChordShapeGuitar[] = [];
+
+    // for each permutation
+    permutationLoop: for (let p of permutations) {
+      // fret numbers
+      const fns = [];
+      // for each
+      for (let i = 0; i < p.length; i++) {
+        const note = p[i];
+        if (note == "x") {
+          // if x push x
+          fns.push("x");
+        } else {
+          // get index of note in chord and note in corresponding string
+          const iChordNote = Maps.sharps.indexOf(note as SupportedNote);
+          const iStringNote = Maps.sharps.indexOf(tuning[i]);
+          // distance between string base note and note in chord permutation
+          const fn = iChordNote - iStringNote;
+          // if negative interval, continue to next iteration
+          if (fn < 0) {
+            continue permutationLoop;
+          }
+          // push fret number
+          fns.push(`${Math.trunc(fn)}`);
+        }
+      }
+      shapes.push(new ChordShapeGuitar(tuning, fns));
+    }
+
+    // return shapes sorted by most playable first (shortest fret span)
+    return shapes.sort((a, b) => a.fretSpan() - b.fretSpan());
+  }
 }
 
 /**
@@ -1888,13 +2239,13 @@ export class Chord {
  */
 export class Note {
   // note frequency
-  readonly frequency: string;
+  readonly frequency: SupportedFrequency;
   /**
    *
    * @param freq frequency key in
    * @throws Error if frequency not in supported frequency list
    */
-  private constructor(freq: string) {
+  private constructor(freq: SupportedFrequency) {
     if (Maps.freqS.indexOf(freq) <= -1) {
       throw new Error("frequency not supported");
     }
@@ -1905,7 +2256,7 @@ export class Note {
    * @param freq note frequency, string or number
    * @returns Note
    */
-  public static fromFreq(freq: string | Number) {
+  public static fromFreq(freq: string | number) {
     const f = Number(freq);
     // if leq first note return first note
     if (f <= Maps.freqN[0]) {
@@ -1937,18 +2288,67 @@ export class Note {
    * @throws Error if note not within supported note range
    * @returns Note
    */
-  public static fromLetter(letter: string) {
+  public static fromLetter(letter: SupportedNote) {
     if (Maps.notes.flat().indexOf(letter) <= -1) {
       throw new Error(
         letter + " note not supported (only support between C0 and B8)"
       );
     }
-    return new Note(letter);
+    return new Note(Maps.noteToFreq[letter]);
   }
   /**
    * @returns stored note letter, if sharp or flat returns array of both
    */
-  public get letter(): string {
+  public get letter(): SupportedNote | [SupportedNote, SupportedNote] {
     return Maps.freqToNote[this.frequency];
+  }
+
+  /**
+   * @returns sharp version of note if accidental
+   */
+  public get sharp(): SupportedNote {
+    const note = Maps.freqToNote[this.frequency];
+    if (typeof note == "object") return note[0];
+    else return note;
+  }
+
+  /**
+   * @returns flat version of note if accidental
+   */
+  public get flat(): SupportedNote {
+    const note = Maps.freqToNote[this.frequency];
+    if (typeof note == "object") return note[1];
+    else return note;
+  }
+}
+
+export class ChordShapeGuitar {
+  readonly tuning: SupportedNote[];
+  readonly fretNumbers: string[];
+
+  constructor(tuning: SupportedNote[], fretNumbers: string[]) {
+    this.tuning = tuning;
+    this.fretNumbers = fretNumbers;
+  }
+
+  /**
+   * @returns distance between lowest and largest fret number, the smaller the better
+   */
+  public fretSpan(): number {
+    // fret numbers array
+    const nums = this.fretNumbers
+      // add numbers to nums
+      .reduce<number[]>((a, v) => {
+        const n = Number(v);
+        if (!isNaN(n)) {
+          a.push(n);
+        }
+        return a;
+      }, [])
+      // sort descending
+      .sort((a, b) => b - a);
+
+    // return max - min, maximum distance in frets
+    return nums[0] - nums[nums.length - 1];
   }
 }
