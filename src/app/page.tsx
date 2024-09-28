44b7068b6b8e62c4ab89881cd5a455cda1ffae8a
shmibblez <watchouthomes@gmail.com>
2024-09-28 05:08:28 -0500

    added chord shape display, minimally functional (still some things to touch up), untested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
-import React, { ReactNode, useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import StartRecordingSVG from "../icons/startRecordingSVG.js"
 import StopRecordingSVG from "../icons/stopRecordingSVG.js"
 import WarningSVG from "../icons/warningSVG.js"
 import LoadingSVG from "../icons/loadingDotsSVG.js"
 import { AudioState, AudioHelper } from "../tools/audioHelper.js"
 import NoSSR from 'react-no-ssr';
+import { Chord, ChordShapeGuitar } from "../tools/chordMaster.js";
 
+// Home, base element
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState("find chords")
   return (
@@ -55,6 +57,7 @@ export default function Home() {
   );
 }
 
+// sidebar tab
 function Tab({ name, selected, setSelected }: { name: string, selected: boolean, setSelected: (s: string) => void }) {
   return (
     <div
@@ -65,6 +68,7 @@ function Tab({ name, selected, setSelected }: { name: string, selected: boolean,
   )
 }
 
+// shows selected tab
 function SelectedTab({ selected }: { selected: string }) {
   switch (selected) {
     case "find chords":
@@ -77,12 +81,16 @@ function SelectedTab({ selected }: { selected: string }) {
 
 /** find chords tab & components */
 
-
+// shows find chords tab
 function FindChordsTab() {
-  const chord: [] = [] // series of notes
-
+  const [chord, setChord] = useState<Chord | null>(null) // series of notes
   const audioHelper = useRef(AudioHelper.instance)
   const [audioState, setAudioState] = useState(audioHelper.current?.currentAudioState)
+  if (audioState === "listening") {
+    audioHelper.current.findChord(6).then(c => {
+      setChord(c)
+    }).catch(e => { console.log(e) })
+  }
 
   useEffect(() => {
     audioHelper.current.audioState.forEach(s => {
@@ -105,6 +113,7 @@ function FindChordsTab() {
   )
 }
 
+// frequency display while listening
 function FrequencyDisplay() {
   const canvas = useRef<HTMLCanvasElement | null>(null)
   const maxAmplitude = useRef<number>(-Infinity);
@@ -192,7 +201,7 @@ function FrequencyDisplay() {
       return requestAnimationFrame(draw)
   }
 
-  var bodyHTML;
+  var bodyHTML: React.JSX.Element;
   if (audioHelper.current.currentAudioState === "loading") {
     bodyHTML = <p>loading...</p>
   } else if (audioHelper.current.currentAudioState === "not supported") {
@@ -222,14 +231,14 @@ function FrequencyDisplay() {
   )
 }
 
-
+// button to record or stop recording
 function RecordButton({ audioState }: { audioState: AudioState }) {
   const audioHelper = useRef<AudioHelper | null>(null)
 
-  var onClick;
-  var bgColor;
-  var buttonText;
-  var iconSVG;
+  var onClick: () => void;
+  var bgColor: string;
+  var buttonText: string;
+  var iconSVG: React.JSX.Element;
 
   useEffect(() => {
     audioHelper.current = AudioHelper.instance
@@ -292,24 +301,253 @@ function RecordButton({ audioState }: { audioState: AudioState }) {
   )
 }
 
-function ChordShapes({ chord }: { chord: [] }) {
+// shows chord shapes when chord found
+function ChordShapes({ chord }: { chord: Chord }) {
+  let body: React.JSX.Element;
+  let hidden = false
+
+  if (chord) {
+    // if chord exists
+    // max chord shapes to show
+    const maxChordShapes = 10
+    let shapes = chord.guitarChordShapes()
+    if (shapes.length <= 0) {
+      // if no shapes generated show message
+      body = (
+        <div className="text-base pl-4 my-auto">
+          <p>{"no chord shapes found"}</p>
+        </div>
+      )
+    } else {
+      // if shapes generated show list of <ChordShape />
+      // maximum fret span for chord
+      const maxFretSpan = 7
+      // limit number of chord shapes shown, taking into account max chord shapes and max fret span
+      if (shapes.length > maxChordShapes) {
+        shapes = shapes.slice(0, maxChordShapes).filter(v => v.fretSpan() <= maxFretSpan)
+      }
+      // max fret span in shapes list
+      const mfs = shapes[shapes.length - 1].fretSpan()
+      // convert chord shapes to visual representation (nFrets is max fret span so same number of frets shown for each)
+      const chordShapes = shapes.map(v =>
+        <li key={v.fretNumbers.join(" ")} style={{ display: "inline" }}>
+          <ChordShape shape={v} nFrets={mfs} />
+        </li>
+      )
+      // body is list of chord shapes
+      body = (
+        <ol className="p-4 bg-zinc-800 rounded-xl border-2 border-zinc-500">
+          {chordShapes}
+        </ol>
+      )
+    }
+  } else {
+    // if chord doesn't exist
+    // TODO: show body according to audio state (listening, error, etc)
+    body = <div />
+    hidden = true
+  }
   return (
     <div
-      style={{ flexGrow: "2" }}>
-      {chordShapeGenerator(chord)}
+      style={{ flexGrow: "2", visibility: hidden ? "hidden" : "visible" }}>
+      {body}
     </div>
   )
 }
 
-function ChordShape({ shape }: { shape: [] }) {
-  return (
-    <div></div>
-  )
-}
+// chord visual representation element
+function ChordShape({ shape, nFrets }: { shape: ChordShapeGuitar, nFrets: number }) {
+  /**
+   * left string thickest (usually string no. 6)
+   * right string thinnest (string no. 1)
+   */
+  const gridElements: React.JSX.Element[] = []
 
-function chordShapeGenerator(notes: []): ReactNode[] {
-  // TODO              
-  return []
+  const numbers = shape.fretNumbers
+    .reduce((a, v) => {
+      const n = Number(v)
+      if (!isNaN(n)) {
+        a.push(n)
+      }
+      return a
+    }, [])
+    // sort ascending
+    .sort((a, b) => a - b)
+  // smallest fret
+  const baseFret = numbers[0]
+  /* draw fret number */
+  gridElements.push(
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      // center at first fret along left
+      gridColumn: `1 / 2`,
+      gridRow: "1 / 3",
+      backgroundColor: "#55ff55",
+    }}>
+      <p>{baseFret}</p>
+    </div>
+  )
+
+  /* draw top fret thicker (bottom + top border below) */
+  gridElements.push(
+    <div
+      style={{
+        // starts at line 2
+        gridColumn: `2 / -1`,
+        // starts at line 1
+        gridRow: `1 / 2`,
+        borderBottom: "3px solid #000000",
+      }}
+    />
+  )
+  // for each fret (fn = fret number)
+  for (let fn = 0; fn < nFrets; fn++) {
+    /* draw each fret (border top for each one) */
+    gridElements.push(
+      <div
+        style={{
+          // starts at line 2
+          gridColumn: `2 / -1`,
+          // starts at line 2
+          gridRow: `${fn + 2} / ${fn + 3}`,
+          borderTop: "3px solid #000000",
+        }}
+      />
+    )
+  }
+
+  // for each string (sn = string number)
+  const stringWidths = [5, 4.5, 4, 3.5, 3, 2.5, 2]
+  for (let sn = 0; sn < shape.tuning.length; sn++) {
+    let fret: string | number = shape.fretNumbers[sn]
+    /* draw each string */
+    gridElements.push(
+      <div
+        style={{
+          // starts at line 2
+          gridColumn: `${sn + 2} / ${sn + 3}`,
+          // starts at line 2
+          gridRow: "2 / -1",
+          backgroundColor: "#ff0000",
+        }}
+      >
+        <div
+          style={{
+            margin: "auto",
+            width: `${stringWidths[sn] ?? 1.5}px`,
+            height: "100%",
+            display: "block",
+            backgroundColor: "#000000",
+          }}
+        ></div>
+      </div>
+    )
+    if (fret == "x") {
+      // if fret x (string muted) TODO: switch for X svg
+      /* draw string letter X */
+      gridElements.push(
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            // starts at column 2
+            gridColumn: `${sn + 2} / ${sn + 3}`,
+            gridRow: "1 / 2",
+            backgroundColor: "#55ff55",
+          }}
+        >
+          <p>{"X"}</p>
+        </div>
+      )
+    } else if (fret == "0") {
+      // if fret 0 (open string)
+      /* draw string letter O TODO: switch for empty circle svg */
+      gridElements.push(
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            // starts at column 2
+            gridColumn: `${sn + 2} / ${sn + 3}`,
+            gridRow: "1 / 2",
+            backgroundColor: "#55ff55",
+          }}
+        >
+          <p>{"O"}</p>
+        </div>
+      )
+    } else {
+      // if fret number
+      /* draw string letter */
+      gridElements.push(
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            // starts at column 2
+            gridColumn: `${sn + 2} / ${sn + 3}`,
+            gridRow: "1 / 2",
+            backgroundColor: "#55ff55",
+          }}
+        >
+          <p>{shape.tuning[sn]}</p>
+        </div>
+      )
+      /* draw finger placement indicator */
+      fret = Number(fret)
+      gridElements.push(
+        <div
+          style={{
+            display: "flex",
+            // starts at line 2
+            gridColumn: `${sn + 2} / ${sn + 3}`,
+            // starts at line 2, lowest fret at this point is 1
+            gridRow: `${baseFret - fret + 1} / ${baseFret - fret + 2}`,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {/* TODO: add solid red circle svg here, width 90% */}
+        </div>
+      )
+    }
+  }
+
+  return (
+    // ratio is long to accomodate all visual elements
+    <div style={{ aspectRatio: 2 / 1 }}>
+      { // TODO: show chord representation with table that show frets,
+        // strings, finger placement (dots), and starting fret number 
+      }
+      {/**
+       * parent grid
+       * layout:
+       *  left side is for starting fret number
+       *  top is for letters
+       *  remaining spaces for strings, frets, and finger placement indicators
+       *  left - right: thick - thin strings
+       *  top - bottom: increasing fret number
+       */}
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "grid",
+          // # columns = 1 + # strings
+          gridTemplateColumns: `1fr repeat(${shape.tuning.length}, 2fr)`,
+          // # frets = 1 + nFrets
+          gridTemplateRows: `1fr repeat(${nFrets + 1}, 2fr)`,
+        }}
+      >
+        {gridElements}
+      </div>
+    </div>
+  )
 }
 
 
