959448f784c166aff37c25e258a4985474df8a08
shmibblez <watchouthomes@gmail.com>
2024-09-29 22:42:15 -0500

    added color options

--- src/icons/loadingDotsSVG.js
+++ src/icons/loadingDotsSVG.js
@@ -1,5 +1,4 @@
-function LoadingDotsSVG() {
-  const stroke = "#ffffff";
+function LoadingDotsSVG({ stroke = "#ffffff" }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
       <circle
