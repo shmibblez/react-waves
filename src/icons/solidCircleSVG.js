8a775a7d96266e32573bb5e38d36c55627f2f7e5
shmibblez <watchouthomes@gmail.com>
2024-09-29 22:41:51 -0500

    added for finger position in chord shape diagrams

---
+++ src/icons/solidCircleSVG.js
@@ -0,0 +1,9 @@
+function SolidCircleSVG({ stroke = "#ffffff" }) {
+  return (
+    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="8" cy="8" r="8" fill={stroke} />
+    </svg>
+  );
+}
+
+export default SolidCircleSVG;
